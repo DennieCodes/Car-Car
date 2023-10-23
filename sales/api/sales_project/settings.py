581b0265102c4e75451230e0437555647b932c59
Dennie <oculareo@gmail.com>
2023-10-23 16:26:53 -0400

    Created and initialized development branch

--- sales/api/sales_project/settings.py
+++ sales/api/sales_project/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-=0#lozw6m8fg901fvz9(b-$@y*_3)v9tgbo9x2se(ezga0)(mj
 DEBUG = True
 
 INSTALLED_APPS = [
+    'sales_rest.apps.SalesRestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
