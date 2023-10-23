8d400eac74ae6a4788f6a6bbc263c948f83b9e21
Anthony Spence <anthonyspence08091973@gmail.com>
2023-10-23 13:28:25 -0700

    started development branch

--- service/api/service_project/settings.py
+++ service/api/service_project/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'sales_rest.apps.SalesRestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
