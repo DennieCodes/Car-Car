1b9385321bef742f3f89c6a3f65e0d1ae78430ec
Dennie <oculareo@gmail.com>
2023-10-26 15:14:33 -0400

    Add list Sales page

--- ghi/app/src/pages/Sales.jsx
+++ ghi/app/src/pages/Sales.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Sales() {
+  const [ salesPersons, setSalesPersons ] = useState("");
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8090/api/sales/");
+
+    if (response.ok) {
+      const data = await response.json();
+
+      setSalesPersons(data.sales)
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
-    <div>Sales</div>
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Employee Id</th>
+            <th>Salesperson Name</th>
+            <th>Customer</th>
+            <th>VIN</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salesPersons && salesPersons.map((sale) => {
+            const { salesperson, customer } = sale;
+            return (
+              <tr key={sale.id}>
+                <td>{salesperson.employee_id}</td>
+                <td>{salesperson.first_name} {salesperson.last_name}</td>
+                <td>{customer.first_name} {customer.last_name}</td>
+                <td>{sale.automobile.vin}</td>
+                <td>{sale.price}</td>
+              </tr>
+            );
+          })}
+					</tbody>
+				</table>
+    </div>
   )
 }
 
