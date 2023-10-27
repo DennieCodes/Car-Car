48f6328c6908748e9d7cafa2c48f35448710b9d3
Dennie <oculareo@gmail.com>
2023-10-27 10:29:45 -0400

    Added List Car Models

--- ghi/app/src/pages/CarModels.jsx
+++ ghi/app/src/pages/CarModels.jsx
@@ -4,12 +4,12 @@ function CarModels() {
   const [ carModels, setCarModels ] = useState("")
 
   const fetchData = async() => {
-    const response = await fetch("http://localhost:8100/api/manufacturers/");
+    const response = await fetch("http://localhost:8100/api/models/");
 
     if (response.ok) {
       const data = await response.json();
 
-      setCarModels(data.manufacturers);
+      setCarModels(data.models);
     }
   };
 
@@ -19,18 +19,23 @@ function CarModels() {
 
   return (
     <div>
-      <h1 className="mt-4">Manufacturers</h1>
+      <h1 className="mt-4">Car Models</h1>
       <table className="table table-striped">
         <thead>
           <tr>
             <th>Name</th>
+            <th>Manufacturer</th>
+            <th>Picture</th>
           </tr>
         </thead>
         <tbody>
-          {carModels && carModels.map((company) => {
+          {carModels && carModels.map((car) => {
+            const { manufacturer } = car
             return (
-              <tr key={company.id}>
-                <td>{company.name}</td>
+              <tr key={car.id}>
+                <td>{car.name}</td>
+                <td>{manufacturer.name}</td>
+                <td><img className="img-fluid" src={car.picture_url} alt={`${manufacturer.name} ${car.name}`}/></td>
               </tr>
             );
           })}
