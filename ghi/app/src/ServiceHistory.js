a804437b03c332f8a1fdbcc1a4ae424d99610010
Dennie <oculareo@gmail.com>
2023-10-29 19:38:49 -0400

    Revised Service History page

--- ghi/app/src/ServiceHistory.js
+++ ghi/app/src/ServiceHistory.js
@@ -1,104 +1,139 @@
 import React from 'react';
-import {useEffect, useState} from "react"
+import { useEffect, useState } from 'react';
 
+function ServiceHistory() {
+	const [service_appointments, setService_Appointments] = useState([]);
+	const [fullAppointments, setFullAppointments] = useState('');
+	const [vin, setVin] = useState('');
 
+	const handleVinChange = (e) => setVin(e.target.value);
+	const [vinList, setVinList] = useState();
 
+	const handleSubmit = (e) => e.preventDefault();
 
+	const fetchData = async () => {
+		const url = 'http://localhost:8100/api/automobiles/';
+		try {
+			const response = await fetch(url);
+			if (response.ok) {
+				const data = await response.json();
+				const vins = data.autos.map((car) => car.vin);
+				setVinList(vins);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-    function ServiceHistory() {
-        const [service_appointments, setService_Appointments] = useState([]);
-        const [vin, setVin] = useState("")
+	const fetchAppointments = async () => {
+		const url = 'http://localhost:8080/api/appointments/';
+		try {
+			const response = await fetch(url);
 
-        const handleVinChange = (e) => setVin(e.target.value)
-        const [vinList, setVinList] = useState()
-        
-        const fetchData = async() => {
-            const url = 'http://localhost:8100/api/automobiles/'
-            try{
-                const response = await fetch(url)
-                if(response.ok){
-                    const data = await response.json()
-                    const vins = data.autos.map((car)=> car.vin)
-                    setVinList(vins)
-            console.log(vins)
-                }
-            } catch(e){
-                console.error(e)
-            }
+			if (response.ok) {
+				const data = await response.json();
+				let { appointments } = data;
 
-        }
-        useEffect(() => {
-            fetchData()
-            fetch('http://localhost:8080/api/appointments/')
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    setService_Appointments(data.appointments);
-                })
-                .catch(e => console.error('error: ', e));
-        }, [])
-    
-    return(
-        <div>
-        <form>
-            <select value = {vin} id = "vin" name = "vin" className = "form-select" required onChange = {handleVinChange}>
-                <option value = "">Search by Vin</option>
-                {vinList && vinList.map((item) => {
-                        return (
-                          <option key={item} value={item}>
-                               {item}
-                          </option>
-                       
-         
-                        )
-                      })}
-            </select>
-            <button className = "btn btn-primary buttons">Search</button>
-        </form>
-        <table className="table table-striped">
-             <thead>
-                 <tr>
-                 <th>Vin</th>
-                 <th>is VIP? </th>
-                 <th>Customer Name</th>
-                 <th>Date</th>
-                 <th>Time</th>
-                 <th>Service Reason</th>
-                 <th>Technician</th>
-                 <th>Status</th>
-                 </tr>   
-            </thead> 
-            <tbody>
-                {service_appointments && service_appointments
-                .filter(service => service.vin === vin)
-                .map(service => {
-                   const {technician,date_time} = service;
+				if (!fullAppointments) {
+					setFullAppointments(appointments);
+				}
 
-                   const dateTime = new Date(date_time);
+				if (vin) {
+					appointments = appointments.filter((ele) => ele.vin === vin);
+				}
+				setService_Appointments(appointments);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-                   const date = dateTime.toISOString().split('T')[0]; // Extracts '2023-11-02'
-                   const time = dateTime.toTimeString().split(' ')[0]; // Extracts '10:40:00'
-                   return (
-   
-                    <tr key={service.id}>
-                       <td>{service.vin}</td>
-                       <td>{service.customer}</td>
-                        <td>{service.dealership_purchase ? "Yes" : "No"}</td>
-                        <td>{date}</td>
-                       <td>{time}</td> 
-                       <td>{service.reason}</td>
-                       <td>{technician.first_name} {technician.last_name}</td>
-                       <td>{service.status}</td>
-                    </tr>
-                        );
-                    })}
-            </tbody> 
-        </table>
+	const handleSubmitClear = () => {
+		setVin('');
+		setService_Appointments(fullAppointments);
+	};
 
-          
-          
-    </div>
-          
-      )
-    }
- export default ServiceHistory
+	const handleSubmitSearch = () => {
+		fetchAppointments();
+	};
+
+	useEffect(() => {
+		fetchData();
+		fetchAppointments();
+	}, []);
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<select
+					value={vin}
+					id="vin"
+					name="vin"
+					className="form-select"
+					onChange={handleVinChange}
+				>
+					<option value="">Search by Vin</option>
+					{vinList &&
+						vinList.map((item) => {
+							return (
+								<option key={item} value={item}>
+									{item}
+								</option>
+							);
+						})}
+				</select>
+				<button
+					onClick={handleSubmitSearch}
+					className="btn btn-primary buttons"
+				>
+					Search
+				</button>
+				<button onClick={handleSubmitClear} className="btn btn-primary buttons">
+					Clear Search
+				</button>
+			</form>
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th>Vin</th>
+						<th>is VIP? </th>
+						<th>Customer Name</th>
+						<th>Date</th>
+						<th>Time</th>
+						<th>Service Reason</th>
+						<th>Technician</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					{service_appointments &&
+						service_appointments
+							// .filter(service => service.vin === vin)
+							.map((service) => {
+								const { technician, date_time } = service;
+
+								const dateTime = new Date(date_time);
+
+								const date = dateTime.toISOString().split('T')[0]; // Extracts '2023-11-02'
+								const time = dateTime.toTimeString().split(' ')[0]; // Extracts '10:40:00'
+								return (
+									<tr key={service.id}>
+										<td>{service.vin}</td>
+										<td>{service.customer}</td>
+										<td>{service.dealership_purchase ? 'Yes' : 'No'}</td>
+										<td>{date}</td>
+										<td>{time}</td>
+										<td>{service.reason}</td>
+										<td>
+											{technician.first_name} {technician.last_name}
+										</td>
+										<td>{service.status}</td>
+									</tr>
+								);
+							})}
+				</tbody>
+			</table>
+		</div>
+	);
+}
+export default ServiceHistory;
