f4b4fb87cbf73c0711bbf390e980e5d08935f27e
Dennie <oculareo@gmail.com>
2023-10-30 08:44:45 -0400

    Updated List Service Appointments page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,57 +1,74 @@
-import a from "../src/pages/pics/a.PNG"
-import shop from "./pages/pics/shop.jpeg"
-import happy from "./pages/pics/happy.jpeg"
-import inventory from "./pages/pics/inventory.jpeg"
-import service from "./pages/pics/service.jpeg"
-import "./index.css"
+import a from '../src/pages/pics/a.PNG';
+import shop from './pages/pics/shop.jpeg';
+import happy from './pages/pics/happy.jpeg';
+import inventory from './pages/pics/inventory.jpeg';
+import service from './pages/pics/service.jpeg';
+import './index.css';
 function MainPage() {
-  return (
-    <div className="px-5 py-1 my-1 text-center">
-      {/* <h1 className="display-5 fw-bold">CarCar</h1> */}
-      <img src={a} class="mx-auto d-block w-50 text-center" id="mainpic"></img> 
-      <div className="col-lg-6 mx-auto">
-        <br></br>
-     
-        <h1 className="lead mb-4"><i><b>The premiere solution for automobile dealership
-          management!</b> </i>
-         
-        </h1>
-      </div>
+	return (
+		<div className="px-5 py-1 my-1 text-center">
+			{/* <h1 className="display-5 fw-bold">CarCar</h1> */}
+			<img
+				src={a}
+				className="mx-auto d-block w-50 text-center"
+				id="mainpic"
+				alt="Auto dealership"
+			></img>
+			<div classNameName="col-lg-6 mx-auto">
+				<br></br>
 
+				<h1 classNameName="lead mb-4">
+					<i>
+						<b>The premiere solution for automobile dealership management!</b>{' '}
+					</i>
+				</h1>
+			</div>
 
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner col-4 text-lg">
+			<div
+				id="carouselExampleSlidesOnly"
+				className="carousel slide"
+				data-bs-ride="carousel"
+			>
+				<div className="carousel-inner col-4 text-lg">
+					<div className="carousel-item active text-lg">
+						<div className="font-weight-bold font-italic text-primary">
+							<h3>Amazing service Department</h3>
+							<img src={shop} className="mx-auto d-block w-75" alt="..."></img>
+						</div>
+					</div>
 
-    <div class="carousel-item active text-lg">
-      <div class="font-weight-bold font-italic text-primary"><h3>Amazing service Department</h3>
-        <img src={shop}class="mx-auto d-block w-75" alt="..."></img>
-      </div>
-      </div>  
-  
-    <div class="carousel-item">
-    <div class="font-weight-bold font-italic text-primary text-lg"><h3>You will love our Inventory!</h3>
-      <img src={happy} class="mx-auto d-block w-75" alt="..."></img>
-    </div>
-    </div>
+					<div className="carousel-item">
+						<div className="font-weight-bold font-italic text-primary text-lg">
+							<h3>You will love our Inventory!</h3>
+							<img src={happy} className="mx-auto d-block w-75" alt="..."></img>
+						</div>
+					</div>
 
-    <div class="carousel-item">
-    <div class="font-weight-bold font-italic text-primary"><h3>You'll love our Sales People!</h3>
-      <img src={service}class="mx-auto d-block w-75" alt="..."></img>
-    </div>
-    </div>
+					<div className="carousel-item">
+						<div className="font-weight-bold font-italic text-primary">
+							<h3>You'll love our Sales People!</h3>
+							<img
+								src={service}
+								className="mx-auto d-block w-75"
+								alt="..."
+							></img>
+						</div>
+					</div>
 
-    <div class="carousel-item">
-    <div class="font-weight-bold font-italic text-primary text-lg"><h3>Come look at our Selection</h3>
-      <img src={inventory} class="mx-auto d-block w-75" alt="..."></img>
-    </div>
-    </div>
-
-    </div>
-  </div>
-</div>
-    
-    
-  );
+					<div className="carousel-item">
+						<div className="font-weight-bold font-italic text-primary text-lg">
+							<h3>Come look at our Selection</h3>
+							<img
+								src={inventory}
+								className="mx-auto d-block w-75"
+								alt="..."
+							></img>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default MainPage;
